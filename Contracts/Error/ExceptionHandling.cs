fbdd4e3d9d0412e56cc61bdb024698503a0df047
mansix27 <mansoureh.asghari@gmail.com>
2023-04-11 20:17:54 +0430

    RemoveContract

--- Contracts/Error/ExceptionHandling.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Mc2.CrudTest.Contracts.Error
-{
-    internal class ExceptionHandling : Exception
-    {
-
-    }
-}
--- Contracts/Error/IErrorMessageLog.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Mc2.CrudTest.Contracts.Error
-{
-    public interface IErrorMessageLog
-    {
-        bool LogError(string layerName, string className, string methodName, string msg);
-    }
-}
